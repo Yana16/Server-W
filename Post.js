e978e9b334c460157dbf45f222a9aeb871295a6f
Yana <example@kr.net>
2024-05-06 21:03:16 +0300

    2
    2

--- Post.js
+++ Post.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Post = new mongoose.Schema({
   userRating: { type: Number, required: true },
-  // postId: { type: Number, required: true },
+  id: { type: String, required: true },
 });
 
 export default mongoose.model("Post", Post);
